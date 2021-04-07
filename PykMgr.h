a01f6986fcd08a781150844cd9e0ae9bd6114805
王一可 <550181213@qq.com>
2021-04-07 17:23:59 +0800

    添加字符串重置函数

--- PykMgr.h
+++ PykMgr.h
@@ -5,6 +5,7 @@ template <UINT nCodePage = 936>
 class CPykMgrTemplate
 {
 public:
+	CPykMgrTemplate() : m_pcData(NULL), m_pwcData(NULL) {}
 	CPykMgrTemplate(const char *pStr) : m_pcData(NULL), m_pwcData(NULL)
 	{
 		if (pStr)
@@ -68,16 +69,66 @@ public:
 		return m_pwcData;
 	}
 
-	~CPykMgrTemplate()
+	void Reset()
 	{
 		if (m_pcData)
 		{
 			delete[]m_pcData;
+			m_pcData = NULL;
 		}
 		if (m_pwcData)
 		{
 			delete[]m_pwcData;
+			m_pwcData = NULL;
 		}
+	}
+
+	CPykMgrTemplate& Reset(const char* pStr)
+	{
+		Reset();
+		if (pStr)
+		{
+			m_pcData = new char[strlen(pStr) + 1];
+			strcpy_s(m_pcData, strlen(pStr) + 1, pStr);
+		}
+		return *this;
+	}
+
+	CPykMgrTemplate& Reset(const wchar_t* pStr)
+	{
+		Reset();
+		if (pStr)
+		{
+			m_pwcData = new wchar_t[wcslen(pStr) + 1];
+			wcscpy_s(m_pwcData, wcslen(pStr) + 1, pStr);
+		}
+		return *this;
+	}
+
+	CPykMgrTemplate& Reset(const char* pStr, const char* pEnd)
+	{
+		Reset();
+		if (pStr && pEnd)
+		{
+			m_pcData = new char[pEnd - pStr + 1];
+			strncpy_s(m_pcData, pEnd - pStr + 1, pStr, pEnd - pStr);
+		}
+		return *this;
+	}
+	CPykMgrTemplate& Reset(const wchar_t* pStr, const wchar_t* pEnd)
+	{
+		Reset();
+		if (pStr)
+		{
+			m_pwcData = new wchar_t[pEnd - pStr + 1];
+			wcsncpy_s(m_pwcData, pEnd - pStr + 1, pStr, pEnd - pStr);
+		}
+		return *this;
+	}
+
+	~CPykMgrTemplate()
+	{
+		Reset();
 	}
 private:
 	char *m_pcData;
